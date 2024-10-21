7aae65802ed859f3a0df8472d097c8e9dafae6c8
José Francisco Mejía Baltodano <valeadasfran30@gmail.com>
2024-10-21 14:25:00 -0600

    Revert "scroll desactivado en sidebar"
    This reverts commit 1f8d96723ff3825398b5092a88d1cad64c419eeb.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import Link from 'next/link';
 import { Container } from './landing/Container';
 import Image from 'next/image';
@@ -19,14 +19,6 @@ export const Header: React.FC<HeaderProps> = ({ links }) => {
   const toggleNavlinks = () => {
     setIsToggled(!isToggled);
   };
-
-  useEffect(() => {
-    if (isToggled) {
-      document.body.classList.add('overflow-hidden');
-    } else {
-      document.body.classList.remove('overflow-hidden');
-    }
-  }, [isToggled]);
 
   return (
     <header>
