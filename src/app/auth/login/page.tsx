b46e6df45713458d04e82cbf61b0e8217fc88634
Jose Cerrato <josecerrato675@gmail.com>
2024-10-21 15:05:53 -0600

    arreglos en logos

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -11,7 +11,7 @@ export default function LoginPage() {
   return (
     <div className=" flex flex-col items-center gap-y-1 pt-3 text-white">
       <LogoWhite height={165} width={165} />
-      <LogoType height={165} width={165} />
+      <LogoType height={89} width={165} />
       <h1 className=" text-2xl sm:text-3xl font-bold mt-3 mb-2" >Inicia Sesión</h1>
       <LoginForm/>
     </div>
